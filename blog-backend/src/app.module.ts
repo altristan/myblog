6bba951f5a1a5863e93fc11c25c7aef54a5b14a1
Al Bandiola <al.bandiola@netzontech.com>
2020-10-07 12:06:15 +0800

    updated docker for heroku deployment; started nestjs authentication using passport module

--- blog-backend/src/app.module.ts
+++ blog-backend/src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://db:27017/nest-blog-project', { useNewUrlParser: true }),
+    // MongooseModule.forRoot('mongodb+srv://altristan:123pass@cluster0.7rokt.mongodb.net/myblog?retryWrites=true&w=majority', { useNewUrlParser: true }),
     BlogModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- blog-backend/src/user/user.module.ts
+++ blog-backend/src/user/user.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+cdimport { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
